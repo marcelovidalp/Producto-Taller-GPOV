a34dc06601793681f20484e09c67dda154d45526
marcelovidalp <marcelo.vidal2023@alu.uct.cl>
2023-11-06 11:21:28 -0300

    Update S_Rutinas.py

--- S_Rutinas.py
+++ S_Rutinas.py
@@ -1,114 +1,124 @@
 import random
 from tkinter import *
 from tkinter import messagebox
-from fpdf import FPDF
-
-ejercicios = [
-    {"nombre": "Sentadillas", "categoria": "Fuerza", "dificultad": "Intermedio", "dolor_relacionado": "Piernas"},
-    {"nombre": "Estiramiento de espalda baja", "categoria": "Movilidad", "dificultad": "Facil", "dolor_relacionado": "Espalda"},
-    {"nombre": "Rotacion de hombros", "categoria": "Movilidad", "dificultad": "facil", "dolor_relacionado": "Hombros"}
-    #MAS EJERCICIOS   
-]
+from reportlab.lib.pagesizes import letter
+from reportlab.pdfgen import canvas
 
 class GeneradorRutinas:
     def __init__(self, ejercicios):
         self.ejercicios = ejercicios
+
+        self.ventana = Tk()
+        self.ventana.title("Generador de Ejercicios.")
+
+        self.frame1 = Frame(self.ventana, bg="dark green", height="400", width="200")
+        self.frame1.pack_propagate(False)
+        self.frame1.config(border = "10")
+        self.frame1.config(relief = SUNKEN)
+        self.frame1.pack()
+        self.frame1.place(x=0, y=0)
+        
+        self.frame2 = Frame(self.ventana, bg="dark green", height="200", width="200", relief=SUNKEN)
+        self.frame2.pack_propagate(False)
+        self.frame2.config(border = "10")
+        self.frame2.config(relief = SUNKEN)
+        self.frame2.pack()
+        self.frame2.place(x=0, y=400)
+
+        self.label_dolor = Label(self.frame1, text="LUGAR DEL DOLOR:", font="BOLD")
+        self.label_dolor.pack()
+        self.label_dolor.place(x=10, y=0)
+        
+        self.label_dolor2 = Label(self.frame1, text="Otro:")
+        self.label_dolor2.pack()
+        self.label_dolor2.place(x=10, y=230)
+        
+        self.entry_dolor = Entry(self.frame1)
+        self.entry_dolor.pack()
+        self.entry_dolor.place(x=50, y=230)
+
+        self.boton1 = Button(self.frame1, text="Piernas.", height="2", width="9")
+        self.boton1.pack()
+        self.boton1.place(x=50, y=50)
+
+        self.boton2 = Button(self.frame1, text="Espalda.", height="2", width="9")
+        self.boton2.pack()
+        self.boton2.place(x=50, y=100)
+
+        self.boton3 = Button(self.frame1, text="Hombros.", height="2", width="9")
+        self.boton3.pack()
+        self.boton3.place(x=50, y=150)
+
+        self.boton4 = Button(self.frame2, text="Necesito Atención Medica.", command=lambda: self.mostrarMensaje())
+        self.boton4.pack()
+        self.boton4.place(x=15, y=70)
+
+        self.boton5 = Button(self.frame1,text="Buscar")
+        self.boton5.pack()
+        self.boton5.place(x=50, y=250)
+
+        self.label_ejercicio = Label(self.ventana, text="", font="BOLD")
+        self.label_ejercicio.pack()
+        self.label_ejercicio.place(x=400, y=75)
+
+        self.label_categoria = Label(self.ventana, text="", font="BOLD")
+        self.label_categoria.pack()
+        self.label_categoria.place(x=399, y=103)
+
+        self.label_dificultad = Label(self.ventana, text="", font="BOLD")
+        self.label_dificultad.pack()
+        self.label_dificultad.place(x=400, y=130)
+
+        self.ventana.geometry("800x600")
+
+    def mostrarEjercicio(self, dolor):
+        rutina = self.generar_rutina(dolor)
+        if rutina:
+            for ejercicio in rutina:
+               self.mostrarDetalle(ejercicio)
+        else:
+            messagebox.showinfo("Generador de Ejercicios", f"No se encontraron ejercicios relacionados con el dolor: {dolor}")
         
     def generar_rutina(self, dolor):
-        rutina = []
         ej_filter = [e for e in self.ejercicios if e['dolor_relacionado'] == dolor]
         if not ej_filter:
             print(f"No se encontraron ejercicios relacionados con el dolor: {dolor}")
-        rutina = random.sample(ej_filter, min(3, len(ej_filter)))
-        return rutina
+        return ej_filter
     
     def guardar_rutina(self, rutina, file, dolor):
-        with open(file, 'a') as archivo_rutina:
-            archivo_rutina.write(f"\n\n############### {dolor} Ejercicio ###############\n\n")
-            for ejercicio in rutina:
-                archivo_rutina.write(f"- Ejercicio: {ejercicio['nombre']}\n- Categoria: {ejercicio['categoria']}\n- Dificultad: {ejercicio['dificultad']}")
-        print(f"Rutina Guardada en {file}")  
-    
+        c = canvas.Canvas(file, pagesize=letter)
+        c.setFont("Helvetica", 12)
+        c.drawString(72, 720, f"----------------- {dolor} Ejercicio ----------------------")
+        for ejercicio in rutina:
+            c.drawString(72, 720, f"- Ejercicio: {ejercicio['nombre']}")
+            c.drawString(72, 720, f"- Categoria: {ejercicio['categoria']}")
+            c.drawString(72, 720, f"- Dificultad: {ejercicio['dificultad']}")
+
+        c.save()
+        print(f"La rutina ha sido guardada en {file}")
+
     def mostrarMensaje(self):
         messagebox.showinfo("Atención Medica", "Acercate a un centro medico para obtener ayuda.")
 
+    def mostrarDetalle(self, ejercicio):
+        self.label_ejercicio.config(text=f"Ejercicio: {ejercicio['nombre']}")
+        self.label_categoria.config(text=f"Categoria: {ejercicio['categoria']}")
+        self.label_dificultad.config(text=f"Dificultad: {ejercicio['dificultad']}")
+
     def main(self):
-        ventana = Tk()
-        ventana.title("Generador de Ejercicios.")
+        self.boton1.config(command=lambda: self.mostrarEjercicio("Piernas"))
+        self.boton2.config(command=lambda: self.mostrarEjercicio("Espalda"))
+        self.boton3.config(command=lambda: self.mostrarEjercicio("Hombros"))
 
-        frame1 = Frame(ventana, bg="LemonChiffon2", height="400", width="200")
-        frame1.pack_propagate(False)
-        frame1.config(border = "10")
-        frame1.config(relief = SUNKEN)
-        frame1.pack()
-        frame1.place(x=0, y=0)
-        
-        frame2 = Frame(ventana, bg="LemonChiffon2", height="200", width="200", relief=SUNKEN)
-        frame2.pack_propagate(False)
-        frame2.config(border = "10")
-        frame2.config(relief = SUNKEN)
-        frame2.pack()
-        frame2.place(x=0, y=400)
+        self.ventana.mainloop()
+ 
 
-        label_dolor = Label(frame1, text="LUGAR DEL DOLOR:", font="BOLD")
-        label_dolor.pack()
-        label_dolor.place(x=0, y=0)
-        
-        label_dolor2 = Label(frame1, text="Otro:")
-        label_dolor2.pack()
-        label_dolor2.place(x=10, y=230)
-        
-        entry_dolor = Entry(frame1)
-        entry_dolor.pack()
-        entry_dolor.place(x=50, y=230)
+ejercicios = [
+    {"nombre": "Sentadillas", "categoria": "Fuerza", "dificultad": "Intermedio", "dolor_relacionado": "Piernas"},
+    {"nombre": "Estiramiento de espalda baja", "categoria": "Movilidad", "dificultad": "Facil", "dolor_relacionado": "Espalda"},
+    {"nombre": "Rotacion de hombros", "categoria": "Movilidad", "dificultad": "Facil", "dolor_relacionado": "Hombros"},
+    {"nombre": "Extensión de pierna", "categoria": "Movilidad", "dificultad": "Facil", "dolor_relacionado": "Piernas"}
+]
 
-        boton1 = Button(frame1, text="Piernas.", height="2", width="9")
-        boton1.pack()
-        boton1.place(x=50, y=50)
-
-        boton2 = Button(frame1, text="Espalda.", height="2", width="9")
-        boton2.pack()
-        boton2.place(x=50, y=100)
-
-        boton3 = Button(frame1, text="Hombros.", height="2", width="9")
-        boton3.pack()
-        boton3.place(x=50, y=150)
-
-        boton4 = Button(frame2, text="Necesito Atención Medica.", command=lambda: self.mostrarMensaje())
-        boton4.pack()
-        boton4.place(x=15, y=70)
-
-        ventana.geometry("800x600")
-        ventana.mainloop()
-'''        
-while True:
-    print("\n-------------MENU PRINCIPAL----------------")
-    print("1.Generar ejercicio.")
-    print("2.Salir")
-    opcion = int(input("Ingrese una opcion: "))
-    
-    if opcion == 1:
-        generador = GeneradorRutinas(ejercicios)
-        dolor = input("Ingrese el dolor del paciente: ")
-        rutina = generador.generar_rutina(dolor)
-        
-        if rutina:
-            print(f"\nRutina generada para el dolor: {dolor}")
-            for ejercicio in rutina:
-                print(f"Ejercicio: {ejercicio['nombre']}\n Categoria: {ejercicio['categoria']}\n Dificultad: {ejercicio['dificultad']}")
-                print("-------------------------------------------------------")
-            subopcion = input(f"\nDesea guardar la rutina? Escriba 'si' o cualquier otro caracter para volver:")
-        
-            if subopcion == "si":
-                generador.guardar_rutina(rutina,'rutinas.txt')
-        else:
-            print("No se encontraron ejercicios relacionados con el dolor ingresado.")
-            
-    elif opcion == 2:
-        print("Saliendo del programa...")
-        break
-    else:
-        print("ERROR, DEBES INGRESAR UNA OPCION VALIDA.\n")
-'''        
 generador = GeneradorRutinas(ejercicios)
 generador.main()
